49f0f28e4c6c1d1b58dbce72529f90f2ad5c92f8
Anton <m@antonz.ru>
2021-05-30 23:56:22 +0300

    sales.sql

---
+++ sales.sql
@@ -0,0 +1,81 @@
+create table sales (
+    year integer,
+    month integer,
+    quarter integer,
+    plan varchar(50),
+    price integer,
+    quantity integer,
+    revenue integer
+);
+insert into sales values(2019,1,1,'silver',60,200,12000);
+insert into sales values(2019,1,1,'gold',240,50,12000);
+insert into sales values(2019,1,1,'platinum',600,10,6000);
+insert into sales values(2019,2,1,'silver',60,660,39600);
+insert into sales values(2019,2,1,'gold',240,60,14400);
+insert into sales values(2019,2,1,'platinum',600,15,9000);
+insert into sales values(2019,3,1,'silver',60,400,24000);
+insert into sales values(2019,3,1,'gold',240,96,23040);
+insert into sales values(2019,3,1,'platinum',600,25,15000);
+insert into sales values(2019,4,2,'silver',60,300,18000);
+insert into sales values(2019,4,2,'gold',240,60,14400);
+insert into sales values(2019,4,2,'platinum',600,18,10800);
+insert into sales values(2019,5,2,'silver',60,440,26400);
+insert into sales values(2019,5,2,'gold',240,80,19200);
+insert into sales values(2019,5,2,'platinum',600,21,12600);
+insert into sales values(2019,6,2,'silver',60,540,32400);
+insert into sales values(2019,6,2,'gold',240,80,19200);
+insert into sales values(2019,6,2,'platinum',600,16,9600);
+insert into sales values(2019,7,3,'silver',60,440,26400);
+insert into sales values(2019,7,3,'gold',240,110,26400);
+insert into sales values(2019,7,3,'platinum',600,30,18000);
+insert into sales values(2019,8,3,'silver',60,440,26400);
+insert into sales values(2019,8,3,'gold',240,99,23760);
+insert into sales values(2019,8,3,'platinum',600,23,13800);
+insert into sales values(2019,9,3,'silver',60,250,15000);
+insert into sales values(2019,9,3,'gold',240,144,34560);
+insert into sales values(2019,9,3,'platinum',600,33,19800);
+insert into sales values(2019,10,4,'silver',60,420,25200);
+insert into sales values(2019,10,4,'gold',240,110,26400);
+insert into sales values(2019,10,4,'platinum',600,19,11400);
+insert into sales values(2019,11,4,'silver',60,495,29700);
+insert into sales values(2019,11,4,'gold',240,55,13200);
+insert into sales values(2019,11,4,'platinum',600,20,12000);
+insert into sales values(2019,12,4,'silver',60,440,26400);
+insert into sales values(2019,12,4,'gold',240,110,26400);
+insert into sales values(2019,12,4,'platinum',600,50,30000);
+insert into sales values(2020,1,1,'silver',60,450,27000);
+insert into sales values(2020,1,1,'gold',240,60,14400);
+insert into sales values(2020,1,1,'platinum',600,12,7200);
+insert into sales values(2020,2,1,'silver',60,1020,61200);
+insert into sales values(2020,2,1,'gold',240,121,29040);
+insert into sales values(2020,2,1,'platinum',600,22,13200);
+insert into sales values(2020,3,1,'silver',60,700,42000);
+insert into sales values(2020,3,1,'gold',240,130,31200);
+insert into sales values(2020,3,1,'platinum',600,28,16800);
+insert into sales values(2020,4,2,'silver',60,700,42000);
+insert into sales values(2020,4,2,'gold',240,180,43200);
+insert into sales values(2020,4,2,'platinum',600,42,25200);
+insert into sales values(2020,5,2,'silver',60,650,39000);
+insert into sales values(2020,5,2,'gold',240,144,34560);
+insert into sales values(2020,5,2,'platinum',600,40,24000);
+insert into sales values(2020,6,2,'silver',60,880,52800);
+insert into sales values(2020,6,2,'gold',240,187,44880);
+insert into sales values(2020,6,2,'platinum',600,54,32400);
+insert into sales values(2020,7,3,'silver',60,780,46800);
+insert into sales values(2020,7,3,'gold',240,168,40320);
+insert into sales values(2020,7,3,'platinum',600,48,28800);
+insert into sales values(2020,8,3,'silver',60,550,33000);
+insert into sales values(2020,8,3,'gold',240,120,28800);
+insert into sales values(2020,8,3,'platinum',600,22,13200);
+insert into sales values(2020,9,3,'silver',60,900,54000);
+insert into sales values(2020,9,3,'gold',240,120,28800);
+insert into sales values(2020,9,3,'platinum',600,23,13800);
+insert into sales values(2020,10,4,'silver',60,950,57000);
+insert into sales values(2020,10,4,'gold',240,150,36000);
+insert into sales values(2020,10,4,'platinum',600,30,18000);
+insert into sales values(2020,11,4,'silver',60,1045,62700);
+insert into sales values(2020,11,4,'gold',240,216,51840);
+insert into sales values(2020,11,4,'platinum',600,60,36000);
+insert into sales values(2020,12,4,'silver',60,1100,66000);
+insert into sales values(2020,12,4,'gold',240,120,28800);
+insert into sales values(2020,12,4,'platinum',600,35,21000);
