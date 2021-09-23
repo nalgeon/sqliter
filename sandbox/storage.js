285c17f4965acc8aacba09064f2cfbd59026cbe1
Anton <m@antonz.org>
2021-09-23 13:05:44 +0300

    sandbox: default query

--- sandbox/storage.js
+++ sandbox/storage.js
@@ -1,6 +1,8 @@
 // Stores various database information
 // in brower storage.
 
+const DEFAULT_QUERY = "select * from sqlite_schema";
+
 // save saves SQL query to the local storage
 function save(database, sql) {
     if (!sql) {
@@ -13,7 +15,7 @@ function save(database, sql) {
 // and sets it as a value for the specified element
 function load(database, el) {
     const sql = localStorage.getItem(`${database}.sql`);
-    el.value = sql || "";
+    el.value = sql || DEFAULT_QUERY;
 }
 
 const storage = { save, load };
